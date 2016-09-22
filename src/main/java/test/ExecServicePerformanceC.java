a73a4346a4eddca8dd8445fd7c1a1f8a7f66c275
quguocheng <quguocheng@meituan.com>
2016-09-22 18:11:00 +0800

    不要使用Math.random()*Math.random()。存在同步竞争。

--- src/main/java/test/ExecServicePerformanceC.java
+++ src/main/java/test/ExecServicePerformanceC.java
@@ -1,20 +1,21 @@
 package test;
 
 import java.util.List;
+import java.util.Random;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
-public class ExecServicePerformanceB {
+public class ExecServicePerformanceC {
 
 	private static int COUNT = 100000;
 
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println(".      priority:"+	Thread.currentThread().getPriority());
+		System.out.println(".      priority:" + Thread.currentThread().getPriority());
 
-		 int cpus = Runtime.getRuntime().availableProcessors();
-		 cpus=16;
+		int cpus = Runtime.getRuntime().availableProcessors();
+//		cpus = 4;
 
 		final ExecutorService es = Executors.newFixedThreadPool(cpus);
 
@@ -56,8 +57,13 @@ public class ExecServicePerformanceB {
 	}
 
 	private static void loop() {
+		/**
+		 * 不要使用Math.random()*Math.random()。因为这种用法只产生一个Random作种子，后续的Math.random()调用都会使用同一个种子。
+		 * 这样，多个线程之间产生竞争，使得ExecutorService的优势完全无法发挥。
+		 */
+		Random r = new java.util.Random();
 		for (int i = 0; i < 2000; i++) {
-			double x = Math.random() * Math.random();
+			double x = r.nextDouble()*r.nextDouble();
 		}
 	}
 
@@ -79,13 +85,32 @@ public class ExecServicePerformanceB {
 	}
 
 	private static void simpleCompuation(int count) {
-		long startTime = System.currentTimeMillis();
-		for (int i = 0; i < count; i++) {
-			loop();
+		Thread t1 = new Thread() {
+			public void run() {
+				long startTime = System.currentTimeMillis();
+				for (int i = 0; i < count; i++) {
+					if (i % 1000 == 0) {
+						long t1 = System.nanoTime();
+						loop();
+						long t2 = System.nanoTime();
+						println("thread:" + Thread.currentThread().getId() + ".      priority:"
+								+ Thread.currentThread().getPriority()
+								+ ".      time consumed:" + (t2 - t1));
+					} else {
+						loop();
+					}
+				}
+				long endTime = System.currentTimeMillis();
+				System.out.println("thread:" + Thread.currentThread().getId() + ".     time end:" + endTime
+						+ ".      time consumed:" + (endTime - startTime));
+			}
+		};
+		t1.start();
+		try {
+			t1.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		long endTime = System.currentTimeMillis();
-		System.out.println("thread:" + Thread.currentThread().getId() + ".     time end:" + endTime
-				+ ".      time consumed:" + (endTime - startTime));
 
 	}
 
@@ -113,18 +138,33 @@ public class ExecServicePerformanceB {
 		public void run() {
 			long startTime = System.currentTimeMillis();
 			int countdown = computations;
-			System.out.println("thread:" + Thread.currentThread().getId() + ".      priority:"+	Thread.currentThread().getPriority()+".     time begin:" + startTime);
+			System.out.println("thread:" + Thread.currentThread().getId() + ".      priority:"
+					+ Thread.currentThread().getPriority() + ".     count:" + countdown + ".     time begin:"
+					+ startTime);
 			/*
 			 * while ( countdown-- > -1 ) { double x = Math.random() *
 			 * Math.random(); }
 			 */
 			for (int i = 0; i < countdown; i++) {
-				loop();
+				if (i % 1000 == 0) {
+					long t1 = System.nanoTime();
+					loop();
+					long t2 = System.nanoTime();
+					println("thread:" + Thread.currentThread().getId() + ".      priority:"
+							+ Thread.currentThread().getPriority() + ".      time consumed:"
+							+ (t2 - t1));
+				} else {
+					loop();
+				}
 			}
 			long endTime = System.currentTimeMillis();
-			System.out.println("thread:" + Thread.currentThread().getId() + ".      priority:"+	Thread.currentThread().getPriority()+".     time end:" + endTime
-					+ ".      time consumed:" + (endTime - startTime));
+			System.out.println("thread:" + Thread.currentThread().getId() + ".      priority:"
+					+ Thread.currentThread().getPriority() + ".     time end:" + endTime + ".      time consumed:"
+					+ (endTime - startTime));
 
 		}
+	}
+	private static void println(String str){
+//		System.out.println(str);
 	}
 }

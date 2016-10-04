a963a25a776d034030d4b548cc652b45b6a44b51
quguocheng <quguocheng@meituan.com>
2016-10-04 20:36:29 +0800

    fix bug

--- fetchUbuntuProcStatus.groovy
+++ fetchUbuntuProcStatus.groovy
@@ -3,7 +3,17 @@ File folder=new File("/proc/${pid}/task/")
 new File("/tmp/${pid}/").mkdir()
 while(folder.exists()){
 	folder.eachFile{
-	    it->new File("/tmp/${pid}/${it.getName()}_status.log") < new File("/proc/${pid}/task/${it.getName()}/status").text
+	    it->
+			def src=new File("/proc/${pid}/task/${it.getName()}/status")
+			def target=new File("/tmp/${pid}/${it.getName()}_status.log")
+			 target<< src.text
+			 println "copy done: ${src} -> ${target} "
+			 
+			 target=new File("/tmp/${pid}/${it.getName()}_final_status.log")
+			 target.write("")
+			 target<<src.text
+			 println "copy done: ${src} -> ${target} "
 	}
 	Thread.sleep(1*1000)
 }
+println "${folder} exits"

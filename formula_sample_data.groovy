fea8edc8e5756cee9cf4bfcfd81525f11aea2c6d
quguocheng <quguocheng@meituan.com>
2016-10-04 11:27:09 +0800

    change param

--- formula_sample_data.groovy
+++ formula_sample_data.groovy
@@ -1,7 +1,7 @@
-Double M=100
+Double M=1000
 def stat=[]
 stat.add(["N", "count", "value"])
-for(int N=1;N<=M*5;N++){
+for(int N=1;N<=M*1.2;N++){
     double count=Math.ceil(M/N)
     double value=count*N
     stat.add([N,count,value])
